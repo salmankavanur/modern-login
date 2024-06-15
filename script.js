2d7e5fad591b0af1a9269587ee8eeb4080873076
Salman MP <mpsalman2011@gmail.com>
2024-06-15 15:20:01 +0530

    restored

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+const signInButton = document.getElementById('login');
+const signUpButton = document.getElementById('register');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
+signInButton.addEventListener('click', () => {
+    container.classList.remove('active');
 });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+signUpButton.addEventListener('click', () => {
+    container.classList.add('active');
 });
